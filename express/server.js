62e165e2239b403a52fb3ede6e05e3e3d7b2c6a7
anYesh Shrestha <sthacruz@gmail.com>
2019-06-14 20:00:31 +0545

    changed content

--- express/server.js
+++ express/server.js
@@ -3,17 +3,54 @@ const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
+const Bot = require("messenger-bot");
 
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.json({
-    hello: "Hello world this is anyesh and i am running express API"
+//  configuring BOT
+let bot = new Bot({
+  token:
+    "EAAOqXvH9M2UBAGNj3VIJsL5XowsA2Ja83ak9IUweWTvZCGWQZBaOHgOUCNxbPNnjivOMALKSZCzKLqFhsWqHEmVCnRAkvvB5egCfPcyZClRuVu8ckLxqyCBRemElzquRAfUFxWvOlX3Khcc6A1Mmwa57nUZBhTbOGPLDPRzSL6gZDZD",
+  verify: "MY_TOKEN",
+  app_secret: "APP_SECRET"
+});
+bot.on("error", err => {
+  console.log(err.message);
+});
+
+bot.on("message", (payload, reply) => {
+  let text = payload.message.text;
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err;
+
+    reply({ text }, err => {
+      if (err) throw JSON.stringify(err);
+
+      console.log(
+        `Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`
+      );
+    });
   });
 });
-router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
-router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.get("/", (req, res) => {
+  return res.json({ server: "server is running" });
+});
+app.get("/bot-verify", (req, res) => {
+  return bot._verify(req, res);
+});
+
+app.post("/", (req, res) => {
+  bot._handleMessage(req.body);
+  res.end(JSON.stringify({ status: "ok" }));
+});
+
 app.use("/.netlify/functions/server", router); // path must route to lambda
 
 module.exports = app;
